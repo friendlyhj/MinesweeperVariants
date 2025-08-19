a53f87582ff1c609b31130229eceac8c35b17c82
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-08-19 10:09:05 +0800

    加入规则SM

---
+++ minesweepervariants/impl/rule/Lrule/SM.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+#
+# @Time    : 2025/06/11 14:25
+# @Author  : xxx
+# @FileName: 1D.py
+"""
+[???]神秘: 这是一条会让男娘感到困惑的规则
+"""
+
+from ....abs.Lrule import AbstractMinesRule
+from ....abs.board import AbstractBoard
+
+
+class Rule1D(AbstractMinesRule):
+    name = ["SM", "???", "神秘"]
+    doc = "这是一条会让男娘感到困惑的规则"
