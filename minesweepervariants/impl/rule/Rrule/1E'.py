cf61a36796da14423f99dbf931c98a1395262b1f
KoolShow <51787949+KoolShow@users.noreply.github.com>
2025-08-20 19:38:39 +0800

    fix: 1E'

--- minesweepervariants/impl/rule/Rrule/1E'.py
+++ minesweepervariants/impl/rule/Rrule/1E'.py
@@ -1,71 +1,207 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#
-# @Time    : 2025/07/09 10:32
-# @Author  : Wu_RH
-# @FileName: 1E.py
+
 """
 [1E'] 视差 (Eyesight')：线索表示纵向和横向的视野之差，箭头指示视野更长的方向
 """
-from typing import Callable, List, Dict
+from typing import Callable
+
+from Cython import cdiv
 
 from ....abs.Rrule import AbstractClueRule, AbstractClueValue
 from ....abs.board import AbstractBoard, AbstractPosition
-from ....utils.image_create import get_row, get_image, get_text, get_col, get_dummy
 
+from ....utils.image_create import get_image, get_text, get_row, get_col, get_dummy
 
 class Rule1E(AbstractClueRule):
     name = ["1E'", "E'", "视差", "Eyesight'"]
-    # DFS遍历存在错误
     doc = "线索表示纵向和横向的视野之差，箭头指示视野更长的方向"
 
     def clue_class(self):
         return Value1E
 
     def fill(self, board: 'AbstractBoard') -> 'AbstractBoard':
+        for pos, _ in board("N"):
+            value = 0
+            # 四方向的函数
+            direction_funcs = [
+                lambda n: type(pos)(pos.x + n, pos.y, pos.board_key),  # 右
+                lambda n: type(pos)(pos.x - n, pos.y, pos.board_key),  # 左
+                lambda n: type(pos)(pos.x, pos.y + n, pos.board_key),  # 上
+                lambda n: type(pos)(pos.x, pos.y - n, pos.board_key)   # 下
+            ]
+
+            for fn in direction_funcs[:2]:  # 只计算横向
+                n = 1
+                while True:
+                    next_pos = fn(n)
+                    if not board.in_bounds(next_pos):
+                        break
+                    if board.get_type(next_pos) == "F":  # 遇到雷，视线被阻挡
+                        break
+                    value += 1
+                    n += 1
+
+            for fn in direction_funcs[2:]:  # 只计算纵向
+                n = 1
+                while True:
+                    next_pos = fn(n)
+                    if not board.in_bounds(next_pos):
+                        break
+                    if board.get_type(next_pos) == "F":  # 遇到雷，视线被阻挡
+                        break
+                    value -= 1
+                    n += 1
+
+            obj = Value1E(pos, bytes([value + 128]))
+            board.set_value(pos, obj)
         return board
-        # fn: Callable[[int], AbstractPosition]
-        # for pos, _ in board("N"):
-        #     value = 0
-        #     for fn in [pos.up, pos.down]:
-        #         n = 1
-        #         while board.get_type(fn(n)) not in "F":
-        #             n += 1
-        #             value += 1
-        #     for fn in [pos.left, pos.right]:
-        #         n = 1
-        #         while board.get_type(fn(n)) not in "F":
-        #             n += 1
-        #             value -= 1
-        #     obj = Value1E(pos, bytes([value + 128]))
-        #     board.set_value(pos, obj)
-        # return board
 
 
 class Value1E(AbstractClueValue):
     def __init__(self, pos: 'AbstractPosition', code: bytes = b''):
-        self.value = code[0] - 128
+        self.value = code[0]
+        self.value = self.value - 128
         self.pos = pos
 
     def __repr__(self):
         return str(self.value)
 
-    def high_light(self, board: 'AbstractBoard') -> List['AbstractPosition']:
+    def high_light(self, board: 'AbstractBoard') -> list['AbstractPosition']:
         positions = []
-        pos = self.pos.clone()
-        for fn in [pos.up, pos.down]:
+        for i in [
+            (1, 0), (-1, 0),
+            (0, 1), (0, -1),
+        ]:
             n = 0
-            while board.get_type(fn(n)) not in "F":
+            while board.get_type(pos := self.pos.shift(i[0] * n, i[1] * n)) not in "F":
                 n += 1
-                positions.append(fn(n))
-        for fn in [pos.left, pos.right]:
-            n = 1
-            while board.get_type(fn(n)) not in "F":
-                n += 1
-                positions.append(fn(n))
+                positions.append(pos)
         return positions
 
-    def compose(self, board, web) -> Dict:
+
+    @classmethod
+    def type(cls) -> bytes:
+        return Rule1E.name[0].encode("ascii")
+
+    def code(self) -> bytes:
+        return bytes([self.value+128])
+
+    def create_constraints(self, board: 'AbstractBoard', switch):
+        model = board.get_model()
+        s = switch.get(model, self)
+
+        direction_funcs = [
+            lambda n: type(self.pos)(self.pos.x + n, self.pos.y, self.pos.board_key),  # 右
+            lambda n: type(self.pos)(self.pos.x - n, self.pos.y, self.pos.board_key),  # 左
+            lambda n: type(self.pos)(self.pos.x, self.pos.y + n, self.pos.board_key),  # 上
+            lambda n: type(self.pos)(self.pos.x, self.pos.y - n, self.pos.board_key)   # 下
+        ]
+
+        def max_steps(fn):
+            n = 1
+            while True:
+                p = fn(n)
+                if not board.in_bounds(p):
+                    return n - 1
+                if board.get_variable(p) is None:
+                    return n - 1
+                n += 1
+
+        def collect_dir(fn, steps):
+            t_positions = []
+            if steps == 0:
+                p_block = fn(1)
+                f_var = board.get_variable(p_block) if board.in_bounds(p_block) else None
+                return t_positions, f_var, True
+
+            for k in range(1, steps + 1):
+                p = fn(k)
+                if not board.in_bounds(p):
+                    return [], None, False
+                var = board.get_variable(p)
+                if var is None:
+                    return [], None, False
+                t_positions.append(p)
+
+            p_block = fn(steps + 1)
+            f_var = board.get_variable(p_block) if board.in_bounds(p_block) else None
+            return t_positions, f_var, True
+
+        max_right = max_steps(direction_funcs[0])
+        max_left = max_steps(direction_funcs[1])
+        max_up = max_steps(direction_funcs[2])
+        max_down = max_steps(direction_funcs[3])
+
+        possible_list = []  # 每项 (set_of_T_positions, list_of_F_vars_or_None)
+
+        max_steps_list = [max_right, max_left, max_up, max_down]
+
+        possible_list = []
+
+        def enum_counts(idx: int, counts: list[int], accum_T: list, accum_F: list):
+            # 计算当前已确定的 delta 与剩余方向的可达范围
+            current_delta = (counts[0] + counts[1]) - (counts[2] + counts[3])
+            # 计算剩余最大可增加的横向与纵向
+            horiz_remain = 0
+            vert_remain = 0
+            for j in range(idx, 4):
+                if j < 2:
+                    horiz_remain += max_steps_list[j]
+                else:
+                    vert_remain += max_steps_list[j]
+
+            min_possible = current_delta - vert_remain
+            max_possible = current_delta + horiz_remain
+            if not (min_possible <= self.value <= max_possible):
+                return
+
+            if idx == 4:
+                if (counts[0] + counts[1]) - (counts[2] + counts[3]) == self.value:
+                    # 复制 accum_F，因为里面可能包含 None
+                    possible_list.append((set(accum_T), list(accum_F)))
+                return
+
+            fn = direction_funcs[idx]
+            max_n = max_steps_list[idx]
+            for steps in range(0, max_n + 1):
+                t_pos, f_var, ok = collect_dir(fn, steps)
+                if not ok:
+                    continue
+
+                # push
+                added = len(t_pos)
+                accum_T.extend(t_pos)
+                accum_F.append(f_var)
+                counts[idx] = steps
+
+                enum_counts(idx + 1, counts, accum_T, accum_F)
+
+                # pop
+                for _ in range(added):
+                    accum_T.pop()
+                accum_F.pop()
+                counts[idx] = 0
+
+        enum_counts(0, [0, 0, 0, 0], [], [])
+
+        tmp_list = []
+        for t_positions, f_vars in possible_list:
+            vars_t = board.batch(t_positions, mode="variable") if t_positions else []
+            vars_f = [v for v in f_vars if v is not None]
+
+            tmp = model.NewBoolVar(f"tmp_1E_{self.pos.x}_{self.pos.y}_{len(tmp_list)}")
+            # 当 tmp 和 线索开关 s 同时成立时，T 位置均为非雷（sum == 0）
+            model.Add(sum(vars_t) == 0).OnlyEnforceIf([tmp, s])
+            # 阻挡位置（若有变量）全部为雷
+            if vars_f:
+                model.AddBoolAnd(vars_f).OnlyEnforceIf([tmp, s])
+            tmp_list.append(tmp)
+
+        if tmp_list:
+            model.AddBoolOr(tmp_list).OnlyEnforceIf(s)
+
+    def compose(self, board, web):
         if self.value == 0:
             return super().compose(board, web)
         if self.value < 0:
@@ -89,68 +225,3 @@ class Value1E(AbstractClueValue):
     @classmethod
     def method_choose(cls) -> int:
         return 1
-
-    @classmethod
-    def type(cls) -> bytes:
-        return Rule1E.name[0].encode("ascii")
-
-    def code(self) -> bytes:
-        return bytes([self.value + 128])
-
-    def create_constraints(self, board: 'AbstractBoard', switch):
-        def dfs(value: int, pos: 'AbstractPosition'):
-            def AddPossibility(_pos: 'AbstractPosition', i: int, j: int):
-                """
-                输入：pos，对应的位置；i：竖着的格子数量；j：横着的格子数量
-                输出：无，但是自动在possible_list里面加上对应的情况
-                """
-                for up in range(i):  # 遍历向上 up 格，可以为 0，不能为 i，注意本身也要算一格
-                    if not (board.in_bounds(_pos.up(up)) and board.in_bounds(_pos.down(i - up - 1))): continue
-                    for right in range(j):  # 遍历向右 right 格，可以为 0，不能为 j，注意本身也要算一格
-                        if (not (board.in_bounds(_pos.right(right)) and
-                                 board.in_bounds(_pos.left(j - right - 1)))): continue
-                        # 到这里的都是符合的情况，开始加 possible_list
-                        lst_1 = []
-                        lst_2 = []
-                        for m in range(up + 1):
-                            if m == up:
-                                if board.in_bounds(_pos.up(m + 1)): lst_2.append(_pos.up(m + 1))
-                            lst_1.append(_pos.up(m))
-                        for m in range((i - up - 1) + 1):
-                            if m == i - up - 1:
-                                if board.in_bounds(_pos.down(m + 1)): lst_2.append(_pos.down(m + 1))
-                            lst_1.append(_pos.down(m))
-                        for m in range(right + 1):
-                            if m == right:
-                                if board.in_bounds(_pos.right(m + 1)): lst_2.append(_pos.right(m + 1))
-                            lst_1.append(_pos.right(m))
-                        for m in range((j - right - 1) + 1):
-                            if m == j - right - 1:
-                                if board.in_bounds(_pos.left(m + 1)): lst_2.append(_pos.left(m + 1))
-                            lst_1.append(_pos.left(m))
-                        if lst_1 == [] and lst_2 == []: continue
-                        possible_list.append((set(lst_1), set(lst_2[:])))
-
-            size = board.get_config(self.pos.board_key, "size")  # 题板尺寸二元组，先行数再列数
-            # 列举符合线索值的情况
-            for i in range(1, size[1] + 1):  # i：竖着能看到的格子数量
-                j = i - value  # j：横着能看到的格子数量
-                if j < 1 or j > size[0]: continue
-                AddPossibility(pos, i, j)
-
-        model = board.get_model()
-        s = switch.get(model, self)
-
-        possible_list = []
-
-        dfs(value=self.value, pos=self.pos)
-        tmp_list = []
-
-        for vars_t, vars_f in possible_list:
-            vars_t = board.batch(vars_t, mode="variable")
-            vars_f = board.batch(vars_f, mode="variable")
-            tmp = model.NewBoolVar("tmp")
-            model.Add(sum(vars_t) == 0).OnlyEnforceIf([tmp, s])
-            model.AddBoolAnd(vars_f).OnlyEnforceIf([tmp, s])
-            tmp_list.append(tmp)
-        model.AddBoolOr(tmp_list).OnlyEnforceIf(s)
